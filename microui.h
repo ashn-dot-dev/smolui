ba2bb76e5b885e2ca68c2275c08c54221a059f75
ashn <me@ashn.dev>
2023-11-02 10:26:18 -0400

    Begin reformatting microui.*

--- microui.h
+++ microui.h
@@ -1,5 +1,6 @@
 /*
 ** Copyright (c) 2020 rxi
+** Copyright (c) 2023 ashn <me@ashn.dev>
 **
 ** This library is free software; you can redistribute it and/or modify it
 ** under the terms of the MIT license. See `microui.c` for details.
@@ -30,79 +31,79 @@
 #define mu_clamp(x, a, b)       mu_min(b, mu_max(a, x))
 
 enum {
-  MU_CLIP_PART = 1,
-  MU_CLIP_ALL
+    MU_CLIP_PART = 1,
+    MU_CLIP_ALL
 };
 
 enum {
-  MU_COMMAND_JUMP = 1,
-  MU_COMMAND_CLIP,
-  MU_COMMAND_RECT,
-  MU_COMMAND_TEXT,
-  MU_COMMAND_ICON,
-  MU_COMMAND_MAX
+    MU_COMMAND_JUMP = 1,
+    MU_COMMAND_CLIP,
+    MU_COMMAND_RECT,
+    MU_COMMAND_TEXT,
+    MU_COMMAND_ICON,
+    MU_COMMAND_MAX
 };
 
 enum {
-  MU_COLOR_TEXT,
-  MU_COLOR_BORDER,
-  MU_COLOR_WINDOWBG,
-  MU_COLOR_TITLEBG,
-  MU_COLOR_TITLETEXT,
-  MU_COLOR_PANELBG,
-  MU_COLOR_BUTTON,
-  MU_COLOR_BUTTONHOVER,
-  MU_COLOR_BUTTONFOCUS,
-  MU_COLOR_BASE,
-  MU_COLOR_BASEHOVER,
-  MU_COLOR_BASEFOCUS,
-  MU_COLOR_SCROLLBASE,
-  MU_COLOR_SCROLLTHUMB,
-  MU_COLOR_MAX
+    MU_COLOR_TEXT,
+    MU_COLOR_BORDER,
+    MU_COLOR_WINDOWBG,
+    MU_COLOR_TITLEBG,
+    MU_COLOR_TITLETEXT,
+    MU_COLOR_PANELBG,
+    MU_COLOR_BUTTON,
+    MU_COLOR_BUTTONHOVER,
+    MU_COLOR_BUTTONFOCUS,
+    MU_COLOR_BASE,
+    MU_COLOR_BASEHOVER,
+    MU_COLOR_BASEFOCUS,
+    MU_COLOR_SCROLLBASE,
+    MU_COLOR_SCROLLTHUMB,
+    MU_COLOR_MAX
 };
 
 enum {
-  MU_ICON_CLOSE = 1,
-  MU_ICON_CHECK,
-  MU_ICON_COLLAPSED,
-  MU_ICON_EXPANDED,
-  MU_ICON_MAX
+    MU_ICON_CLOSE = 1,
+    MU_ICON_CHECK,
+    MU_ICON_COLLAPSED,
+    MU_ICON_EXPANDED,
+    MU_ICON_MAX
 };
 
 enum {
-  MU_RES_ACTIVE       = (1 << 0),
-  MU_RES_SUBMIT       = (1 << 1),
-  MU_RES_CHANGE       = (1 << 2)
+    MU_RES_ACTIVE       = (1 << 0),
+    MU_RES_SUBMIT       = (1 << 1),
+    MU_RES_CHANGE       = (1 << 2)
 };
 
 enum {
-  MU_OPT_ALIGNCENTER  = (1 << 0),
-  MU_OPT_ALIGNRIGHT   = (1 << 1),
-  MU_OPT_NOINTERACT   = (1 << 2),
-  MU_OPT_NOFRAME      = (1 << 3),
-  MU_OPT_NORESIZE     = (1 << 4),
-  MU_OPT_NOSCROLL     = (1 << 5),
-  MU_OPT_NOCLOSE      = (1 << 6),
-  MU_OPT_NOTITLE      = (1 << 7),
-  MU_OPT_HOLDFOCUS    = (1 << 8),
-  MU_OPT_AUTOSIZE     = (1 << 9),
-  MU_OPT_POPUP        = (1 << 10),
-  MU_OPT_CLOSED       = (1 << 11),
-  MU_OPT_EXPANDED     = (1 << 12)
+    MU_OPT_ALIGNCENTER  = (1 << 0),
+    MU_OPT_ALIGNRIGHT   = (1 << 1),
+    MU_OPT_NOINTERACT   = (1 << 2),
+    MU_OPT_NOFRAME      = (1 << 3),
+    MU_OPT_NORESIZE     = (1 << 4),
+    MU_OPT_NOSCROLL     = (1 << 5),
+    MU_OPT_NOCLOSE      = (1 << 6),
+    MU_OPT_NOTITLE      = (1 << 7),
+    MU_OPT_HOLDFOCUS    = (1 << 8),
+    MU_OPT_AUTOSIZE     = (1 << 9),
+    MU_OPT_POPUP        = (1 << 10),
+    MU_OPT_CLOSED       = (1 << 11),
+    MU_OPT_EXPANDED     = (1 << 12)
 };
 
 enum {
-  MU_MOUSE_LEFT       = (1 << 0),
-  MU_MOUSE_RIGHT      = (1 << 1),
-  MU_MOUSE_MIDDLE     = (1 << 2)
+    MU_MOUSE_LEFT       = (1 << 0),
+    MU_MOUSE_RIGHT      = (1 << 1),
+    MU_MOUSE_MIDDLE     = (1 << 2)
 };
 
 enum {
-  MU_KEY_SHIFT        = (1 << 0),
-  MU_KEY_CTRL         = (1 << 1),
-  MU_KEY_ALT          = (1 << 2),
-  MU_KEY_BACKSPACE    = (1 << 3),
-  MU_KEY_RETURN       = (1 << 4)
+    MU_KEY_SHIFT        = (1 << 0),
+    MU_KEY_CTRL         = (1 << 1),
+    MU_KEY_ALT          = (1 << 2),
+    MU_KEY_BACKSPACE    = (1 << 3),
+    MU_KEY_RETURN       = (1 << 4)
 };
 
 
@@ -124,94 +125,93 @@ typedef struct { mu_BaseCommand base; mu_Font font; mu_Vec2 pos; mu_Color color;
 typedef struct { mu_BaseCommand base; mu_Rect rect; int id; mu_Color color; } mu_IconCommand;
 
 typedef union {
-  int type;
-  mu_BaseCommand base;
-  mu_JumpCommand jump;
-  mu_ClipCommand clip;
-  mu_RectCommand rect;
-  mu_TextCommand text;
-  mu_IconCommand icon;
+    int type;
+    mu_BaseCommand base;
+    mu_JumpCommand jump;
+    mu_ClipCommand clip;
+    mu_RectCommand rect;
+    mu_TextCommand text;
+    mu_IconCommand icon;
 } mu_Command;
 
 typedef struct {
-  mu_Rect body;
-  mu_Rect next;
-  mu_Vec2 position;
-  mu_Vec2 size;
-  mu_Vec2 max;
-  int widths[MU_MAX_WIDTHS];
-  int items;
-  int item_index;
-  int next_row;
-  int next_type;
-  int indent;
+    mu_Rect body;
+    mu_Rect next;
+    mu_Vec2 position;
+    mu_Vec2 size;
+    mu_Vec2 max;
+    int widths[MU_MAX_WIDTHS];
+    int items;
+    int item_index;
+    int next_row;
+    int next_type;
+    int indent;
 } mu_Layout;
 
 typedef struct {
-  mu_Command *head, *tail;
-  mu_Rect rect;
-  mu_Rect body;
-  mu_Vec2 content_size;
-  mu_Vec2 scroll;
-  int zindex;
-  int open;
+    mu_Command *head, *tail;
+    mu_Rect rect;
+    mu_Rect body;
+    mu_Vec2 content_size;
+    mu_Vec2 scroll;
+    int zindex;
+    int open;
 } mu_Container;
 
 typedef struct {
-  mu_Font font;
-  mu_Vec2 size;
-  int padding;
-  int spacing;
-  int indent;
-  int title_height;
-  int scrollbar_size;
-  int thumb_size;
-  mu_Color colors[MU_COLOR_MAX];
+    mu_Font font;
+    mu_Vec2 size;
+    int padding;
+    int spacing;
+    int indent;
+    int title_height;
+    int scrollbar_size;
+    int thumb_size;
+    mu_Color colors[MU_COLOR_MAX];
 } mu_Style;
 
 struct mu_Context {
-  /* callbacks */
-  int (*text_width)(mu_Font font, const char *str, int len);
-  int (*text_height)(mu_Font font);
-  void (*draw_frame)(mu_Context *ctx, mu_Rect rect, int colorid);
-  /* core state */
-  mu_Style _style;
-  mu_Style *style;
-  mu_Id hover;
-  mu_Id focus;
-  mu_Id last_id;
-  mu_Rect last_rect;
-  int last_zindex;
-  int updated_focus;
-  int frame;
-  mu_Container *hover_root;
-  mu_Container *next_hover_root;
-  mu_Container *scroll_target;
-  char number_edit_buf[MU_MAX_FMT];
-  mu_Id number_edit;
-  /* stacks */
-  mu_stack(char, MU_COMMANDLIST_SIZE) command_list;
-  mu_stack(mu_Container*, MU_ROOTLIST_SIZE) root_list;
-  mu_stack(mu_Container*, MU_CONTAINERSTACK_SIZE) container_stack;
-  mu_stack(mu_Rect, MU_CLIPSTACK_SIZE) clip_stack;
-  mu_stack(mu_Id, MU_IDSTACK_SIZE) id_stack;
-  mu_stack(mu_Layout, MU_LAYOUTSTACK_SIZE) layout_stack;
-  /* retained state pools */
-  mu_PoolItem container_pool[MU_CONTAINERPOOL_SIZE];
-  mu_Container containers[MU_CONTAINERPOOL_SIZE];
-  mu_PoolItem treenode_pool[MU_TREENODEPOOL_SIZE];
-  /* input state */
-  mu_Vec2 mouse_pos;
-  mu_Vec2 last_mouse_pos;
-  mu_Vec2 mouse_delta;
-  mu_Vec2 scroll_delta;
-  int mouse_down;
-  int mouse_pressed;
-  int key_down;
-  int key_pressed;
-  char input_text[32];
+    /* callbacks */
+    int (*text_width)(mu_Font font, const char *str, int len);
+    int (*text_height)(mu_Font font);
+    void (*draw_frame)(mu_Context *ctx, mu_Rect rect, int colorid);
+    /* core state */
+    mu_Style _style;
+    mu_Style *style;
+    mu_Id hover;
+    mu_Id focus;
+    mu_Id last_id;
+    mu_Rect last_rect;
+    int last_zindex;
+    int updated_focus;
+    int frame;
+    mu_Container *hover_root;
+    mu_Container *next_hover_root;
+    mu_Container *scroll_target;
+    char number_edit_buf[MU_MAX_FMT];
+    mu_Id number_edit;
+    /* stacks */
+    mu_stack(char, MU_COMMANDLIST_SIZE) command_list;
+    mu_stack(mu_Container*, MU_ROOTLIST_SIZE) root_list;
+    mu_stack(mu_Container*, MU_CONTAINERSTACK_SIZE) container_stack;
+    mu_stack(mu_Rect, MU_CLIPSTACK_SIZE) clip_stack;
+    mu_stack(mu_Id, MU_IDSTACK_SIZE) id_stack;
+    mu_stack(mu_Layout, MU_LAYOUTSTACK_SIZE) layout_stack;
+    /* retained state pools */
+    mu_PoolItem container_pool[MU_CONTAINERPOOL_SIZE];
+    mu_Container containers[MU_CONTAINERPOOL_SIZE];
+    mu_PoolItem treenode_pool[MU_TREENODEPOOL_SIZE];
+    /* input state */
+    mu_Vec2 mouse_pos;
+    mu_Vec2 last_mouse_pos;
+    mu_Vec2 mouse_delta;
+    mu_Vec2 scroll_delta;
+    int mouse_down;
+    int mouse_pressed;
+    int key_down;
+    int key_pressed;
+    char input_text[32];
 };
-
 
 mu_Vec2 mu_vec2(int x, int y);
 mu_Rect mu_rect(int x, int y, int w, int h);
